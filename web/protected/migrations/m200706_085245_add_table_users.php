d0769d57697fbcda48e1483eadabfcf563e3efad
Max Protodyakonov <zigfried123@mail.ru>
2020-07-07 20:24:43 +0400

    added migrations

---
+++ web/protected/migrations/m200706_085245_add_table_users.php
@@ -0,0 +1,15 @@
+<?php
+
+class m200706_085245_add_table_users extends CDbMigration
+{
+	public function up()
+	{
+	    $this->execute('CREATE TABLE user (id INTEGER NOT NULL AUTO_INCREMENT, username VARCHAR(20) NOT NULL, password CHAR(6) NOT NULL, PRIMARY KEY(id))');
+	}
+
+	public function down()
+	{
+        $this->dropTable('user');
+	}
+
+}
---
+++ web/protected/migrations/m200707_131514_add_log_table.php
@@ -0,0 +1,15 @@
+<?php
+
+class m200707_131514_add_log_table extends CDbMigration
+{
+	public function up()
+	{
+        $this->execute('CREATE TABLE log_access (id INT NOT NULL AUTO_INCREMENT, ip VARCHAR(20) NOT NULL, date INT NOT NULL, data VARCHAR(100) NOT NULL, PRIMARY KEY(id))');
+	}
+
+	public function down()
+	{
+		$this->dropTable('log_access');
+	}
+
+}
